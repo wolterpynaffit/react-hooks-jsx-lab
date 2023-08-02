08c5da479d8892f1c7b8e91c031731e349a0aee0
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-01 21:26:52 -0400

    finished

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { image } from "../data/data";
-
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div id="about">About
+        <h2>About Me</h2>
+        <p> n this lab, we will be rendering a l our code in the NavBar.js, Home.js, and About.js files. These files are all located in the src/components directory</p>
+        <img src={image} alt="I made this"></img>
+      </div>
+    </>
+  )
+  
+
 }
 
 export default About;
